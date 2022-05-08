5f80651589bda30199fbd46d95a7b27e1d0e199a
chandeok <rajivqcnstg@gmail.com>
2022-05-09 01:44:40 +0300

    added codlimate badge

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from "readline-sync";
 export const hello = () => {
   console.log("Welcome to the Brain Games!");
-  console.log("May I have your name? ");
-  const name = readlineSync.question();
+  const name = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${name}!`);
 };
